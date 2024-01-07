21d2526811b49b6c9404b714f5d3b0eb56369e28
AVSaiKumar <ms2204101012@gmail.com>
2024-01-08 00:13:46 +0530

    Initial commit

---
+++ offlineService/offlineServiceApp.swift
@@ -0,0 +1,17 @@
+//
+//  offlineServiceApp.swift
+//  offlineService
+//
+//  Created by A V SAI KUMAR REDDY on 03/01/24.
+//
+
+import SwiftUI
+
+@main
+struct offlineServiceApp: App {
+    var body: some Scene {
+        WindowGroup {
+            ContentView()
+        }
+    }
+}
